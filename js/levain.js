34f45a6289ab46b6bcbb99f07877f9f8d058d575
kashiwaya3900 <62826538+kashiwaya3900@users.noreply.github.com>
2020-11-26 23:46:20 +0900

    グリッドレイアウト変更

--- js/levain.js
+++ js/levain.js
@@ -33,3 +33,26 @@ function Lottery() {
   alert("Hello");
 }
 
+
+function AllChecked(){
+    var all = document.form.all.checked;
+    for (var i=0; i<document.form.part.length; i++){
+      document.form.part[i].checked = all;
+    }
+  }
+
+// 一つでもチェックを外すと「全て選択」のチェック外れる
+function DisChecked(){
+  var checks = document.form.part;
+  var checksCount = 0;
+  for (var i=0; i<checks.length; i++){
+    if(checks[i].checked == false){
+      document.form.all.checked = false;
+    }else{
+      checksCount += 1;
+      if(checksCount == checks.length){
+        document.form.all.checked = true;
+      }
+    }
+  }
+}
