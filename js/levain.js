a1b7dffe6695898e4b0528afd07836917e8f1580
kashiwaya3900 <62826538+kashiwaya3900@users.noreply.github.com>
2020-11-29 22:18:00 +0900

    グリッドレイアウト変更

--- js/levain.js
+++ js/levain.js
@@ -91,18 +91,278 @@ function TypeSelected(){
 function Lottery() {
   console.log('Lottery');
   
+  //選択メンバーリスト
+  var select_member = [];
+  
   var elements = document.getElementsByName("part");
   for (let i=0; i<elements.length; i++){
     if (elements[i].checked){
-      console.log(elements[i].value);
+      //名前をPUSHする
+      select_member.push(elements[i].defaultValue);
     }
   }
   
-  //
+  var radioVal = $("input[name='type']:checked").val();
   
-  //シングルス
+  if(radioVal == "singles") {
+    //シングルス
+    //2人以上選択していないとシングルスは不可
+    if(select_member.length < 2){
+      alert("2人以上選択してね");
+      return;
+    }
+    
+    //シャッフル
+    var shuffle_list = ShuffleList(select_member);
+    var round_robin_list = CreateRoundRobin(shuffle_list);
+    
+    var beforePlayer1 = "";
+    var beforePlayer2 = "";
+    var loopCount = round_robin_list.length;
+    
+    var success_list = [];
+    var success = false;
+    var offset=0;
+    
+    for (var i=0; i<loopCount; i++){
+        var player1 = round_robin_list[i][0];
+        var player2 = round_robin_list[i][1];
+        
+        var res_list = RoundRobinSinglesSort(round_robin_list,offset,player1,player2,beforePlayer1,beforePlayer2,success_list)
+        
+        
+        //List[i]を取得する
+        var player1 = round_robin_list[offset][0];
+        var player2 = round_robin_list[offset][1];
+        
+        
+      
+      
+    }
+    
+    
+  }else if(radioVal == "doubles"){
+    //ダブルス
+    //4人以上選択していないとシングルスは不可
+     if(select_member.length < 4){
+      alert("4人以上選択してね");
+      return;
+    }
+    //シャッフル
+    var shuffle_list = ShuffleList(select_member);
+  }else{
+    //団体戦
+    //10人以上選択していないと団体は不可
+    if(select_member.length < 10){
+      alert("10人以上選択してね");
+      return;
+    }
+    //シャッフル
+    var shuffle_list = ShuffleList(select_member);
+    //doubles1
+    $('div.group_doubles1').html(ResultCreateDoubles(shuffle_list[0],shuffle_list[1],shuffle_list[2],shuffle_list[3]));
+    //doubles2
+    $('div.group_doubles2').html(ResultCreateDoubles(shuffle_list[4],shuffle_list[5],shuffle_list[6],shuffle_list[7]));
+    //singles1
+    $('div.group_singles').html(ResultCreateSingles(shuffle_list[8],shuffle_list[9]));
+    
+    
+    
+  }
+
+}
+
+/**
+ * 配列のシャッフル処理
+ */
+function ShuffleList(list) {
+  newList = [];
+
+  while (list.length > 0) {
+    n = list.length;
+    k = Math.floor(Math.random() * n);
+
+    newList.push(list[k]);
+    list.splice(k, 1);
+  }
   
-  //ダブルス
+  return newList;
+}
+
+/**
+ * 総当たり作成処理
+ */
+function CreateRoundRobin(list){
+  let member1 = list.concat();
+  let member2 = list.concat();
   
-  //団体戦
+  var round_robin = [];
+  
+  for(let m1 of member1){
+    member2.shift();
+    for(let m2 of member2){
+        console.log(m1 + ' vs ' + m2);
+        round_robin.push([m1,m2,'']);
+    }
+  }
+  return round_robin;
+}
+
+/**
+ * 試合順のソート(singles)
+ */
+function RoundRobinSinglesSort(round_robin_list,offset,player1,player2,beforePlayer1,beforePlayer2,success_list) {
+  for (var i=0; i<round_robin_list.length; i++){
+    //beforeのペアと比較する
+    if((player1 != beforePlayer1 && player1 != beforePlayer2) && (player2 != beforePlayer1 && player2 != beforePlayer2)) {
+      //処理済リストに入っているかどうか
+      if(success_list.indexOf(i) < 0){
+        //登録可
+        console.log(player1 + ":" +player2);
+        beforePlayer1 = player1;
+        beforePlayer2 = player2;
+        success_list.push(j);
+        success = true;
+        break;
+      }
+    }
+  }
+  if(!success){
+    for (var k=0; k<loopCount; k++){
+      //処理済リストに入っていない
+      if(success_list.indexOf(k) < 0){
+        //登録可
+        var player1 = round_robin_list[k][0];
+        var player2 = round_robin_list[k][1];
+        console.log(player1 + ":" +player2);
+        beforePlayer1 = player1;
+        beforePlayer2 = player2;
+        success_list.push(j);
+        offset = k + 1;
+        success = true;
+      }
+    }
+  }
+}
+
+/**
+ * 試合順のソート(doubles)
+ */
+function RoundRobinDoublesSort(list) {
+  
+}
+
+
+function ResultCreateSingles(player1,player2){
+  var singles = "";
+  
+  //性別を取得
+  var gender1;
+  var gender2;
+  
+   gender1 = memMap.get(player1) == '0' ? "result_member_male" : "result_member_female";
+   gender2 = memMap.get(player2) == '0' ? "result_member_male" : "result_member_female";
+
+  
+  singles = '<div class="row">'
+  + '<div class="col-4">'
+  + '<div class="row">'
+  + '<div class="col-12">'
+  + '<div class="centering">'
+  + '<label class="'
+  + gender1
+  + '">'
+  + player1
+  + '</label>'
+  + '</div>'
+  + '</div>'
+  + '</div>'
+  + '</div>'
+  + '<div class="col-4  align-self-center">'
+  + '<label class="vs">VS</label>'
+  + '</div>'
+  + '<div class="col-4">'
+  + '<div class="row">'
+  + '<div class="col-12">'
+  + '<div class="centering">'
+  + '<label class="'
+  + gender2
+  + '">'
+  + player2
+  + '</label>'
+  + '</div>'
+  + '</div>'
+  + '</div>'
+  + '</div>'
+  + '</div>';
+  
+  return singles;
+}
+
+function ResultCreateDoubles(player1,player2,player3,player4){
+  var doubles = "";
+  
+  //性別を取得
+  var gender1;
+  var gender2;
+  var gender3;
+  var gender4;
+  
+   gender1 = memMap.get(player1) == '0' ? "result_member_male" : "result_member_female";
+   gender2 = memMap.get(player2) == '0' ? "result_member_male" : "result_member_female";
+   gender3 = memMap.get(player3) == '0' ? "result_member_male" : "result_member_female";
+   gender4 = memMap.get(player4) == '0' ? "result_member_male" : "result_member_female";
+
+  
+  doubles = '<div class="row">'
+  + '<div class="col-4">'
+  + '<div class="row">'
+  + '<div class="col-12">'
+  + '<div class="centering">'
+  + '<label class="'
+  + gender1
+  + '">'
+  + player1
+  + '</label>'
+  + '</div>'
+  + '</div>'
+  + '<div class="col-12">'
+  + '<div class="centering">'
+  + '<label class="'
+  + gender2
+  + '">'
+  + player2
+  + '</label>'
+  + '</div>'
+  + '</div>'
+  + '</div>'
+  + '</div>'
+  + '<div class="col-4  align-self-center">'
+  + '<label class="vs">VS</label>'
+  + '</div>'
+  + '<div class="col-4">'
+  + '<div class="row">'
+  + '<div class="col-12">'
+  + '<div class="centering">'
+  + '<label class="'
+  + gender3
+  + '">'
+  + player3
+  + '</label>'
+  + '</div>'
+  + '</div>'
+  + '<div class="col-12">'
+  + '<div class="centering">'
+  + '<label class="'
+  + gender4
+  + '">'
+  + player4
+  + '</label>'
+  + '</div>'
+  + '</div>'
+  + '</div>'
+  + '</div>'
+  + '</div>';
+  
+  return doubles;
 }
