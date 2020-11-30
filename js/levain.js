d08c67f865448d6c1f9f4faa9f60a56be9cf15a8
kashiwaya3900 <62826538+kashiwaya3900@users.noreply.github.com>
2020-11-30 22:46:14 +0900

    グリッドレイアウト変更

--- js/levain.js
+++ js/levain.js
@@ -2,6 +2,7 @@ var memMap;
 var selectMemMap;
 
 Init();
+no_scaling();
 /**
  *初期処理
  */
@@ -423,4 +424,30 @@ function ResultCreateDoubles(player1,player2,player3,player4){
   + '</div>';
   
   return doubles;
+}
+
+
+/**
+* タッチ操作での拡大縮小禁止
+*/
+function no_scaling() {
+    document.addEventListener("touchmove", mobile_no_scroll, { passive: false });
+}
+
+/**
+* タッチ操作での拡大縮小禁止解除
+*/
+function return_scaling() {
+    document.removeEventListener('touchmove', mobile_no_scroll, { passive: false });
+}
+
+/**
+* 拡大縮小禁止
+*/
+function mobile_no_scroll(event) {
+    // ２本指での操作の場合
+    if (event.touches.length >= 2) {
+        // デフォルトの動作をさせない
+        event.preventDefault();
+    }
 }
